98401f45b92ec60c2c58133127c238b0a9ec6f20
joel <joel@gmail.com>
2021-08-10 22:45:44 -0500

    add the script file and library jstl - jdbc

--- src/java/config/Conexion.java
+++ src/java/config/Conexion.java
@@ -3,17 +3,19 @@ package config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
     Connection con;
     String url="jdbc:mysql://localhost:3306/bd_ventas";
     String user="root";
-    String pass="12345678";
+    String pass="root";
     public Connection Conexion(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
             con=DriverManager.getConnection(url,user,pass);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error:"+e);
         }
         return con;
     }
